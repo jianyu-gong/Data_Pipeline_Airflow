75e6e083befebccecc114f4f7f9b80c3c6ed7f1b
Jianyu Gong <jianyu0503@gmail.com>
2019-07-21 22:33:12 -0400

    update DAG and Plug
    1st submission

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -11,12 +11,28 @@ class LoadFactOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_source="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_source = sql_source
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Deleteing existing tables from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+
+        formatted_sql = LoadDimensionOperator.insert_sql.format(
+            self.table,
+            self.sql_source
+        )
+        self.log.info(f"Executing {formatted_sql} ...")
+        redshift.run(formatted_sql)
